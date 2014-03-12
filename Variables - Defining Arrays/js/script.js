ac8c6f4407ff0f1f93e71b685e3908c5692475de
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-11 20:01:55 -0700

    Completed Screencast: Arrays.

---
+++ Variables - Defining Arrays/js/script.js
@@ -0,0 +1,10 @@
+// Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+
+muppetNames[1] = "Rizzo"; //change index #1 to Rizzo
+muppetNames[2] = "Fozzy";
+
+var num = 1;
+
+console.log(muppetNames[num]); //lets me see what's in index #1
