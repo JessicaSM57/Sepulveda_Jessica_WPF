cbee2f8cff582c0f9a06250f1867430ba4cd795c
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-11 23:16:16 -0700

    Completed Slice of Pie part II.

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -1,8 +1,10 @@
 //Jessica Sepulveda -- 3/1/14 -- Expressions Worksheet
 
-//Slice of Pie per person
-var pizzaPies = 10;
+//Slice of Pie part II
+var pizzaPies = 4;
 var pieSlices = 8;
-var partyGuests = 40;
+var partyGuests = 10;
 var slicesPer = pizzaPies * pieSlices/partyGuests; //Find out how many pizza slices each guest can eat.
-console.log("Each person ate " + slicesPer + " slices of pizza at the party."); //Prints out how many slices of pizza each person can eat.
+var totalSlices = pizzaPies * pieSlices; //Find out how many slices there are total.
+var remainder = totalSlices % partyGuests; //Calculates the remainder using the module operator.
+console.log("Sparky got " + remainder + " slices of pizza."); //Prints out how many slices of pizza each person can eat.
