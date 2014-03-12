c620d685dfe3ca092563f78845ad8101b36d90f7
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-11 22:53:27 -0700

    Completed Slices of Pie 1 exercise.

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -1,7 +1,8 @@
 //Jessica Sepulveda -- 3/1/14 -- Expressions Worksheet
 
-//Sparky's age in dog years.
-var a = 1; //Sparky is 1 year old
-var b = a + 7; //add 7 "dog years" to a
-var sparkysAge = "Sparky is " + a + " human year old which is " + b + " years old in dog years."; //Assigns a calculation to the resulting variable along with text.
-console.log(sparkysAge); //Prints out Sparky's age in dog years.
+//Slice of Pie per person
+var pizzaPies = 10;
+var pieSlices = 8;
+var partyGuests = 40;
+var slicesPer = pizzaPies * pieSlices/partyGuests; //Find out how many pizza slices each guest can eat.
+console.log("Each person ate " + slicesPer + " slices of pizza at the party."); //Prints out how many slices of pizza each person can eat.
