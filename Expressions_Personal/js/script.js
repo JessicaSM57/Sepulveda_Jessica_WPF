2538112fa55e35d2eb25289e8add1d50c1bacd62
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 21:30:18 -0700

    Completed first expression to meet rubric specifications.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -11,9 +11,10 @@ var eatingOut = prompt("How much did you spend on eating out in February?");
 var entertain = prompt("How much money did you spend on entertainment in February?");
 var other = prompt("How much did you spend on other items including alcohol, gaming, hair cuts, hobbies, and recreation?");
 
-//Create an equation that answers the question posed.
+//Create an equation that answers the original question posed.
 var totalSpent = Number(eatingOut) + Number(entertain) + Number(other); //Casting the variables as Numbers allows for addition.
 var dif = totalSpent / income * 100 - 5; //Calculates percentage spent frivolously and compares it to the recommendation.
+var recommended = income * .05; //Calculates how much should have been spent.
 
 //Set up the detailed answer the detailed answer in console, but it can be easily changed to alert mode.
-//console.log("You spent $" + totalSpent + " on frivolous items in February; which is " + dif + "% more than the 5% recommended. In a month when you make $" + income + " you should only spend $" + recommended + "on the aforementioned discretionary items.");
+console.log("You spent $" + totalSpent + " on frivolous items in February; which is " + dif + "% more than the 5% recommended. In a month when you make $" + income + " you should only spend $" + recommended + " on the aforementioned discretionary items.");
