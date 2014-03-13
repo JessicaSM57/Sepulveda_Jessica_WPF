2c5bda1030a6bc63c4da9960f624fefe4b9c6d79
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 19:51:46 -0700

    Final example of how prompts can be used; this one makes a var result which provides another way to concatenate strings

--- Prompt & Alert/js/script.js
+++ Prompt & Alert/js/script.js
@@ -3,8 +3,10 @@
 //var userInput = prompt("Enter your year of birth:"); //Need var in order to record it into the log.
 //    console.log(userInput);
 
-//Another way to print out result for the user to view is by using alert as follows.
+//Another way to get to the result is by making it a var.
 var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width."); //To enter to next line use \n
 var height = prompt("Please enter the height.");
 var area = width * height; //Calculates with that information
-alert("The area of your rectangle is " + area + "sq feet.");
+var result = ("The area of your rectangle is " + area + " sq feet."); //Another way to concatenate string.
+alert(result);
+
