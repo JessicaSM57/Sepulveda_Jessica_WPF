5fd714005d5e1e27a0a72d4598d0b3bb1aafab90
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 19:39:32 -0700

    Completed Total amount spent on groceries (at another computer without SmartGit so copied/pasted code here).

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -1,10 +1,8 @@
 //Jessica Sepulveda -- 3/1/14 -- Expressions Worksheet
 
-//Slice of Pie part II
-var pizzaPies = 4;
-var pieSlices = 8;
-var partyGuests = 10;
-var slicesPer = pizzaPies * pieSlices/partyGuests; //Find out how many pizza slices each guest can eat.
-var totalSlices = pizzaPies * pieSlices; //Find out how many slices there are total.
-var remainder = totalSlices % partyGuests; //Calculates the remainder using the module operator.
-console.log("Sparky got " + remainder + " slices of pizza."); //Prints out how many slices of pizza each person can eat.
+//Total Amount spent on groceries
+
+var subTotals = [80,90,70,65,57];  // Five weekly grocery totals
+var totalSum = subTotals[0] + subTotals[1] + subTotals[2] + subTotals[3] + subTotals[4]; //Use array to add up total.
+var average = totalSum/5; //Find out average by dividing by total number of items in array.
+console.log("You have spent a total of $" + totalSum + " on groceries over 5 weeks. That is an average of $" + average + " per week."); //Prints grand total and average.
