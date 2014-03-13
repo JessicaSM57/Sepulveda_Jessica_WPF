067b265fce73a050f09d212cab7b453dbd462ae0
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 22:19:33 -0700

    Set up question and established initial array.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,8 +1,8 @@
 //Jessica Sepulveda -- March 12, 2014 -- Assignment: Expressions
 
-//Wacky: How many cats will I have in my lifetime?
+//Wacky: How much food will my dogs consume in their lifetime?
 
 //Create prompt to establish age.
-var age = prompt ("How old are you?");
-
-var past = prompt ("How many cats have you had?");
+var sub = [prompt("How much do your dog(s) weigh cumulatively?"), prompt("What is your dog(s) remaining life expectancy?"), prompt("3")]; //This is an array.
+var totalSub = Number(sub[0]) + Number(sub[1]) + Number(sub[2]); //
+    console.log(totalSub);
