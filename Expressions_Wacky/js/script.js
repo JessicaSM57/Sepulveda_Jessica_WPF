fe217b97e48a6b8577a35960096dcd84f36ce455
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 21:39:08 -0700

    Set up expression and started sorting through logistics.

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,8 @@
+//Jessica Sepulveda -- March 12, 2014 -- Assignment: Expressions
+
+//Wacky: How many cats will I have in my lifetime?
+
+//Create prompt to establish age.
+var age = prompt ("How old are you?");
+
+var past = prompt ("How many cats have you had?");
