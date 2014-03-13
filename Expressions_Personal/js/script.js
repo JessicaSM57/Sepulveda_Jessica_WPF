39be66a952b0d65db21508dc30ff42bd102b2874
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 20:15:51 -0700

    Started working on code but having trouble getting it to identify numbers as such versus text; appears to be stuck on wanting to use + sign to concatenate.

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,16 @@
+//Jessica Sepulveda -- March 12, 2014 -- Assignment: Expressions
+
+//Personal: Are I spending money frivolously?
+
+//Create prompt to establish givens and introduce data collection:
+var income = prompt("According to NoMoreDebts.org, you should only spend 5% of your income on eating out, entertainment, and other discretionary expenses. \nPlease enter your adjusted monthly income for February.");
+
+//Collect information from user:
+var eatingOut = prompt("How much did you spend on eating out in February?");
+var entertain = prompt("How much money did you spend on entertainment in February?");
+var other = prompt("How much did you spend on other items including alcohol, gaming, hair cuts, hobbies, and recreation?");
+
+//Built expressions using addition, division, and subtraction.
+//var entry = [eatingOut, entertain, other]; //Built an array so the 3 entries can be treated as numbers versus text.
+//var total = [entry[0] + entry[1] + entry[2]];
+    console.log(total);
