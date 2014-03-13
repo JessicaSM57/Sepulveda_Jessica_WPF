40458bd8fccdd4ea405f3d182f95be4b4ab9933c
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 19:40:56 -0700

    Completed Discounts example (at another computer without GitHub so copied/pasted my completed code here and double checked it on HTML).

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -1,8 +1,13 @@
 //Jessica Sepulveda -- 3/1/14 -- Expressions Worksheet
 
-//Total Amount spent on groceries
+//Discounts
 
-var subTotals = [80,90,70,65,57];  // Five weekly grocery totals
-var totalSum = subTotals[0] + subTotals[1] + subTotals[2] + subTotals[3] + subTotals[4]; //Use array to add up total.
-var average = totalSum/5; //Find out average by dividing by total number of items in array.
-console.log("You have spent a total of $" + totalSum + " on groceries over 5 weeks. That is an average of $" + average + " per week."); //Prints grand total and average.
+var itemDesc = ["jacket"]; //Item is given and described.
+var originalPrice = 157.99; //Original price of item is given and specified.
+var discountPercentage = ["25%"]; //Discount percentage is given and specified.
+var discount = 25/100; //Discount percentage is converted for mathematical processing.
+var discountedPrice = originalPrice-originalPrice*discount; //Built expression for discounted price of item: 157.99-39.4975=118.4925
+var salesTax = .0825; //Sales tax is given.
+var afterTax = salesTax*discountedPrice+discountedPrice; //Built expression for discounted price of item after tax: 118.4925*.0825=9.7756 ; 9.7756+118.4925=128.2681
+console.log("Your " + itemDesc + " was originally $" + originalPrice + ", but after a discount of " + discountPercentage + ", it is now $" + discountedPrice + " without tax, and $" + afterTax + " with tax.");
+
