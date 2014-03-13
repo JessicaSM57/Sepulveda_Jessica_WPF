6239367eabbb27b24000ad1f3918f60b69477696
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 19:49:29 -0700

    First excercise for prompt & alert (completed at another computer and copied/pasted my code here, then double checked).

--- Prompt & Alert/js/script.js
+++ Prompt & Alert/js/script.js
@@ -1,4 +1,10 @@
 //Prompt and Alert Screencast
 
-var userInput = prompt("Enter your year of birth:"); //Need var in order to record it into the log.
-    console.log(userInput);
+//var userInput = prompt("Enter your year of birth:"); //Need var in order to record it into the log.
+//    console.log(userInput);
+
+//
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width."); //To enter to next line use \n
+var height = prompt("Please enter the height.");
+var area = width * height; //Calculates with that information
+console.log(area); //Prints it out to console.
