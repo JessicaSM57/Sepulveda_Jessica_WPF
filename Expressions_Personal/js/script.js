89ffde1c4c94136ffe1169e1e6eb788611086b22
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 21:19:45 -0700

    Changed result from showing as alert to showing in console log per rubric.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -11,10 +11,10 @@ var eatingOut = prompt("How much did you spend on eating out in February?");
 var entertain = prompt("How much money did you spend on entertainment in February?");
 var other = prompt("How much did you spend on other items including alcohol, gaming, hair cuts, hobbies, and recreation?");
 
-//Create an equation that calculates how much money you have left in the month.
-var totalLeft = income - eatingOut - entertain - other;
-var totalSpent = income - totalLeft;
-var dif = totalSpent / income * 100 - 5;
+//Create an equation that answers the question posed.
+var totalLeft = income - eatingOut - entertain - other; //Used subtraction instead of addition due to unresolvable complication with getting numbers to identify as digits versus text when using addition.
+var totalSpent = income - totalLeft; //Extra step needed due to using subtraction instead of addition.
+var dif = totalSpent / income * 100 - 5; //Calculates percentage spent frivolously and compares it to the recommendation.
 
-//Set up the alert with the answer.
-alert("You spent $" + totalSpent + " on frivolous items in February; which is " + dif + "% more than the 5% recommended.");
+//Set up the detailed answer the detailed answer in console, but it can be easily changed to alert mode.
+console.log("You spent $" + totalSpent + " on frivolous items in February; which is " + dif + "% more than the 5% recommended.");
