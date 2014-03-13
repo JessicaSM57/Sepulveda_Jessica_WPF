a21f58c63065ef7bda883a2e3ba623f0772837df
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 22:34:03 -0700

    Beginning to configure arithmetic equation to solve problem.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,7 +2,10 @@
 
 //Wacky: How much food will my dogs consume in their lifetime?
 
-//Create prompt to establish age.
-var sub = [prompt("How much do your dog(s) weigh cumulatively?"), prompt("What is your dog(s) remaining life expectancy?"), prompt("3")]; //This is an array.
-var totalSub = Number(sub[0]) + Number(sub[1]) + Number(sub[2]); //
+//Create prompt in an array format in order to establish age weight, life expectancy, and names.
+var questions = [prompt("How much do your dog(s) weigh cumulatively?"), prompt("What is your dog(s) remaining life expectancy?"), prompt("What are your dog's names?")]; //This is an array.
+
+//
+var food = Number(questions[0]) * 24 *
+    Number(sub[1]) + Number(sub[2]); //
     console.log(totalSub);
