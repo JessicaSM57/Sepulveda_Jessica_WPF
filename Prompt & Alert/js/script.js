df6537c2fbadfc1bb5dad28e5403ef2d3ddff290
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 19:50:30 -0700

    Second example of prompting with answer included in detailed explanation.

--- Prompt & Alert/js/script.js
+++ Prompt & Alert/js/script.js
@@ -3,8 +3,8 @@
 //var userInput = prompt("Enter your year of birth:"); //Need var in order to record it into the log.
 //    console.log(userInput);
 
-//
+//Another way to print out result for the user to view is by using alert as follows.
 var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width."); //To enter to next line use \n
 var height = prompt("Please enter the height.");
 var area = width * height; //Calculates with that information
-console.log(area); //Prints it out to console.
+alert("The area of your rectangle is " + area + "sq feet.");
