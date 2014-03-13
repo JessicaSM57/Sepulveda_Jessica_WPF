7cf3ce94f5da953c38d9bfa4675bd47fae06ac96
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 20:33:22 -0700

    I could not get addition to work correctly so I slightly modified the arithmetic to compensate. This is an issue I need to figure out later as it consumed most of my day.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,12 +5,16 @@
 //Create prompt to establish givens and introduce data collection:
 var income = prompt("According to NoMoreDebts.org, you should only spend 5% of your income on eating out, entertainment, and other discretionary expenses. \nPlease enter your adjusted monthly income for February.");
 
+
 //Collect information from user:
 var eatingOut = prompt("How much did you spend on eating out in February?");
 var entertain = prompt("How much money did you spend on entertainment in February?");
 var other = prompt("How much did you spend on other items including alcohol, gaming, hair cuts, hobbies, and recreation?");
 
-//Built expressions using addition, division, and subtraction.
-//var entry = [eatingOut, entertain, other]; //Built an array so the 3 entries can be treated as numbers versus text.
-//var total = [entry[0] + entry[1] + entry[2]];
-    console.log(total);
+//Create an equation that calculates how much money you have left in the month.
+var totalLeft = income - eatingOut - entertain - other;
+var totalSpent = (income - totalLeft) / income;
+var totalPercent = totalSpent
+
+//Set up the alert with the answer.
+//alert ("You spent $" + totalSpent + "on frivolous items in February; which is " + + "% more than the 5% recommended.");
