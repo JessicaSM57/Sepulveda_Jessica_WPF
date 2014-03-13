ecde15dd07e8b1e2610b25455db0387facfae1e1
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-12 23:00:48 -0700

    Commented on code to clarify ounces.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,11 +3,12 @@
 //Wacky: How much food will my dogs consume in their lifetime?
 
 //Create prompt in an array format in order to establish age weight, life expectancy, and names.
-var questions = [prompt("How much do your dog(s) weigh cumulatively?"), prompt("What is your dog(s) remaining life expectancy?"), prompt("What are your dog's names?")]; //This is an array.
+var questions = [prompt("How much do your dog(s) weigh cumulatively in pounds?"), prompt("What is your dog(s) remaining life expectancy in years?"), prompt("What are your dog's names?")]; //This is an array.
 
-//Given that a 40 pound dog consumes 3 cups of food per day the following equation is established:
-var dailyFood = Number(questions[0]) / 40 * 3;
+//Given that a 40 pound dog consumes 24 ounces of food per day the following equation is established:
+var dailyFood = (Number(questions[0]) / 40) * 24; //Example: 2 pound dog / 40 pound dog * 24 ounces = 1.2 ounces of food per day
 
-//Using life expectancy entered, this expression converts years to days and then multiplies
 
-    console.log(food);
+//Using life expectancy entered, this expression converts years to days and then multiplies dailyFood and finally converts to pounds.
+//var answer = Number(questions[1]) * 365 * dailyFood / 16; //Example: 2 years of life expectancy for a 2 pound dog =
+    console.log(dailyFood);
