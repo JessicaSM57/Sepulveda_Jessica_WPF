4f51ba54f578e93c0d348e5fb7c902c82e1ec181
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-13 18:45:02 -0700

    Set up 3 prompts in an array.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,3 +2,5 @@
 
 //Professional: What is your student retention rate this month?
 
+//Create prompts to introduce data collection and establish givens while setting up an array.
+var questions = [prompt("How many students do you currently have?"), prompt("How many students will be graduating this month?"), prompt("How many students do you expect to withdraw this month?")];
