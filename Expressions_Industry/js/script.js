2b92d70169677dd7aa0d5ecd14f502660c262b87
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-13 19:10:37 -0700

    Edited HTML and script.js files.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,6 +1,6 @@
 //Jessica Sepulveda -- March 13, 2014 -- Assignment: Expressions
 
-//Professional: What is your student retention rate this month?
+//Professional: What is your student retention rate?
 
 //Create prompts to introduce data collection and establish givens while setting up an array.
 var questions = [prompt("How many students do you currently have?"), prompt("How many students will be graduating this month?"), prompt("How many students do you expect to withdraw this month?")];
