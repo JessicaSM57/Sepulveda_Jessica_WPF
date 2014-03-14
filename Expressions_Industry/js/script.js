b0da71b9093c5e6a56eba467d17164893c9cc17d
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-13 18:46:47 -0700

    Created formula to calculate monthly retention rate.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,3 +4,7 @@
 
 //Create prompts to introduce data collection and establish givens while setting up an array.
 var questions = [prompt("How many students do you currently have?"), prompt("How many students will be graduating this month?"), prompt("How many students do you expect to withdraw this month?")];
+
+//Create formula to calculate monthly retention rate while keeping in mind PEMDA, then multiply by 100 to get percentage.
+var total = (questions[0] - questions[1] - questions[2]) / questions[0] * 100;
+console.log("Your student retention rate for this month will be " + total + "%."); //Print results to console to fulfill assignment requirement although an alert would be more applicable.
