4d5f7aac9142bd1102dc7b7c780dd4d4c9e61f5c
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-16 14:04:45 -0700

    Added an if statement to print a result to console when the condition is not true.

--- Conditionals Screencast/js/script.js
+++ Conditionals Screencast/js/script.js
@@ -1,12 +1,14 @@
-//Conditional Logic -- With an Expression
+//Conditional Logic -- If and else : Choosing between 2 blocks of code
 
 var kidHeight = 47;
 var minHeight = 48;
-var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
+}
+if(kidHeight < minHeight){
+   console.log("Sorry kid, you've got some growing to do first!");
 }
