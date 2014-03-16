66e60166285bf82128859400134f056297880367
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-16 14:20:41 -0700

    else if statement added as a second condition

--- Conditionals Screencast/js/script.js
+++ Conditionals Screencast/js/script.js
@@ -1,16 +1,19 @@
 //Conditional Logic -- else if
 //Choosing between three or more blocks of code
-//if ALWAYS requires a condition but else NEVER should have a condition
+//if ALWAYS requires a condition
 
 var kidHeight = 52;
 var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
 if(kidHeight > minHeight){
-    //code performed if condition is true
+    //print out you can ride
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //print out you can ride with a parent present
+   console.log("You can ride, but only with a parent present.");
 }else{
-    //code performed if condition is false
-   console.log("Sorry kid, you've got some growing to do first!");
+    console.log("Sorry kid, you've got some growing to do first!");
 }
