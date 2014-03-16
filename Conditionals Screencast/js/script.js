c2752a9fa91bf907e610f62451ccd1996158cda9
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-16 15:50:46 -0700

    Specific example setting up if else conditional logic

--- Conditionals Screencast/js/script.js
+++ Conditionals Screencast/js/script.js
@@ -1,9 +1,10 @@
 //Conditional Logic -- Ternary Operators
 
-if(condition){
-    do if true;
-}else{
-    do if false;
-}
+var gpa = 48;
 
-(condition) ? do if true : do if false;
+//if the gpa is over the min 2.0 score, the student can graduate
+if(gpa > 2.0){
+    console.log("You can graduate!");
+}else{
+    consoloe.log("GPA is too low!");
+}
