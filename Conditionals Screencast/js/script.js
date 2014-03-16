25fa0b00426bf1efb03c7ed8018feb3efe588fe5
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-16 16:04:13 -0700

    Notated code to clarify best practices for ternary and traditional conditional statements.

--- Conditionals Screencast/js/script.js
+++ Conditionals Screencast/js/script.js
@@ -1,10 +1,10 @@
 //Conditional Logic -- Ternary Operators
 
-var age = 11;
+var age = 6;
 var book;
 
 //If the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
 
 book = (age < 10 ? "Green Eggs and Ham" : "The Time Machine");
-
+//Ternary condition is able to return a value into the variable book. This is easier and more elegant than using a huge conditional statements in long form. If you're dealing with really large blocks of code, then you will want to build more traditional conditional statements. If you are dealing with simple assignment, then ternary works best.
 console.log(book);
