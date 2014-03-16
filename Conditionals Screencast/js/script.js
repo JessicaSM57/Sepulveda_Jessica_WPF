70764c83c739fedd49f58d38f33c1e4461be47cd
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-16 15:48:53 -0700

    Set up to show how to combine if else statements into a condition

--- Conditionals Screencast/js/script.js
+++ Conditionals Screencast/js/script.js
@@ -1,12 +1,9 @@
-//Conditional Logic -- Logical Operators
+//Conditional Logic -- Ternary Operators
 
-var budget = 100;
-var iPhonePrice = 199.99;
-var wonLottery = true;
-
-//
-if(iPhonePrice < budget || wonLottery){ // === sign is not necessary and it is more elegant to leave it out
-    console.log("We can buy the phone!");
+if(condition){
+    do if true;
 }else{
-    console.log("No phone for you!");
+    do if false;
 }
+
+(condition) ? do if true : do if false;
