4f5394b9edc3533f1eb34b3319087dc5dd3e5af1
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-16 15:16:40 -0700

    && is for going in between relational expressions not to be used as an operator

--- Conditionals Screencast/js/script.js
+++ Conditionals Screencast/js/script.js
@@ -5,7 +5,7 @@ var iPhonePrice = 199.99
 var paycheck = 200
 
 //if the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice && paycheck < 300){ //&& is for going in between relational expressions not to be used as an operator
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
