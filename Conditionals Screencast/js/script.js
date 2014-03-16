550e9ca37178c89bdcf1c4b730f280ae3d3c5fbb
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-16 13:31:53 -0700

    Started Relational Expressions Sreencast. First result, kid is under 48 when condition requires height to be greater than 48, so nothing prints to console.

--- Conditionals Screencast/js/script.js
+++ Conditionals Screencast/js/script.js
@@ -1,10 +1,10 @@
-//Conditional Logic
+//Conditional Logic -- Relational Expressions
 
-var oldEnough = false;
+var kidHeight = 30;
 
 //if the child is old enough, print to the console "you can ride!"
-if(oldEnough){
+//if the kid is over 48 inches in height
+if(kidHeight > 48){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
-console.log("what comes after"); //This statement is printed to console but what was printed before it is ignored. 
