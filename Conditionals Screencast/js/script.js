ec66f9e85e3d92dc575f7bdcc1593dc858da3570
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-16 14:15:10 -0700

    Set up to follow screencast for else if in order to set up 3 blocks or more.

--- Conditionals Screencast/js/script.js
+++ Conditionals Screencast/js/script.js
@@ -1,6 +1,8 @@
-//Conditional Logic -- With an Expression -- Else
+//Conditional Logic -- else if
+//Choosing between three or more blocks of code
+//if ALWAYS requires a condition but else NEVER should have a condition
 
-var kidHeight = 48;
+var kidHeight = 52;
 var minHeight = 48;
 
 //if the child is old enough, print to the console "you can ride!"
