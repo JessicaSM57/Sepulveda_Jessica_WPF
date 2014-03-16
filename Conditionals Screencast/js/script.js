c201f2dbeb1d55ae754256eca6c1328e1206bd9d
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-16 15:58:07 -0700

    Conditional statement is set up with if else

--- Conditionals Screencast/js/script.js
+++ Conditionals Screencast/js/script.js
@@ -1,13 +1,13 @@
 //Conditional Logic -- Ternary Operators
 
-var gpa = 1.3;
+var age = 11;
+var book;
 
-//if the gpa is over the min 2.0 score, the student can graduate
-/*if(gpa > 2.0){
-    console.log("You can graduate!");
-//}else{
-    consoloe.log("GPA is too low!");
-}*/
+//If the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
 
-//built the above in ternary form
-(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
+if(age < 10){
+    book = "Green Eggs and Ham";
+}else{
+    book = "The Time Machine";
+}
+console.log(book);
