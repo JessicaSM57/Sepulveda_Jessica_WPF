27c97291aa1b19692d6134d5eb6d3743868904e9
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-16 15:38:53 -0700

    Shows === should not be used unless absolutely necessary, which it is definitely not with an OR statement.

--- Conditionals Screencast/js/script.js
+++ Conditionals Screencast/js/script.js
@@ -4,8 +4,8 @@ var budget = 100;
 var iPhonePrice = 199.99;
 var wonLottery = true;
 
-// OR operator states that as long as one of these things is true then the whole thing is going to be true; if we win the lottery then it doesnt matter what the budget is going to be; the OR operator only needs 1 of these to be true in order for the whole thing to be true
-if(iPhonePrice < budget || wonLottery === true){
+//
+if(iPhonePrice < budget || wonLottery){ // === sign is not necessary and it is more elegant to leave it out
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
