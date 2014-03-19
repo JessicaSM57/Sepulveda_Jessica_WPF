2715ab7586d6061c52df409505bbf0e2eb551360
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-18 22:32:17 -0700

    Set up variables but need to include a conditional statement.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,22 @@
 //Jessica Sepulveda -- 03/18/14 -- Conditionals Assignment
 
-//Personal
+//Personal: Fun Run
+//Given: Amount raised is matched by 10% for each 10 minutes that child runs. Max time to run allotted is 30 minutes.
+
+//Set up variables that will be used in calculations.
+
+var amount = prompt("How much money did you raise?");
+    //Validate that an entry is made:
+    if(amount == ""){
+    amount = prompt("You did not enter a number. Please enter amount a whole numeric value.")
+    }
+
+var match = prompt("How long did you run?");
+    //Validate that an entry is made:
+    if(match == ""){
+    match = prompt("You did not enter a number. Please enter minutes in a whole numeric value.")
+    }
+
+
+
+//Ternary at end: prize = (totalRaised >= 100 ? "trophy" : "medal");
