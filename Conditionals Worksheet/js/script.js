db664e8b4f45e5845cf53dae2d31fe05079f1d29
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-18 17:37:34 -0700

    Set up if else condition to provide results.

--- Conditionals Worksheet/js/script.js
+++ Conditionals Worksheet/js/script.js
@@ -5,4 +5,8 @@
 
 var weight = 250; //Participant must be 250 pounds minimum to qualify
 
-
+if(weight >= 250){//Person must be 250 pounds or more to qualify
+    console.log("The competitor qualifies for the heavyweight division.");
+}else{ //Sets up condition for result if below 250 pounds
+    console.log("The competitor needs to gain some weight!");
+}
