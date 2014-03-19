ee324a58dfe65f690f2136db060d2f0d14628d88
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-18 17:42:36 -0700

    Completed Stuff Your Face example.

--- Conditionals Worksheet/js/script.js
+++ Conditionals Worksheet/js/script.js
@@ -6,7 +6,7 @@
 var weight = prompt("Please enter your weight."); //Participant must be 250 pounds minimum to qualify
 
 if(weight >= 250){//Person must be 250 pounds or more to qualify
-    console.log("The competitor qualifies for the heavyweight division.");
+    console.log("The competitor qualifies for the heavyweight division."); //Prints if this condition is met
 }else{ //Sets up condition for result if below 250 pounds
-    console.log("The competitor needs to gain some weight!");
+    console.log("The competitor needs to gain some weight!"); //Prints if the condition is NOT met
 }
