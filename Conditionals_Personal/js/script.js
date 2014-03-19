b6ff13e11f0a911516b654a26254ca1bb653c657
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-18 23:02:29 -0700

    Successfully set up conditional using if, else if, else as well as concluding ternary.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,6 +2,7 @@
 
 //Personal: Fun Run
 //Given: Amount raised is incrementally increased by 10% for each 10 minutes that child runs. Max time to run allotted is 30 minutes.
+//Given:
 
 //Set up variables that will be used in calculations.
 
@@ -13,17 +14,18 @@ var amount = prompt("How much money did you raise?");
 
 var time = prompt("How long did you run?");
     //Validate that an entry is made:
-    if(time >=){
+    if(time == ""){
     time = prompt("You did not enter a number. Please enter minutes in a whole numeric value.");
     }
 
 //Set up conditions to be applied
-if(time <= 9){
-    console.log("You ran for " + time + " minutes so you raised $" + amount + " . Try harder next year!");
+if(time >= 11 && time <= 20){
+    console.log("You ran for " + time + " minutes so we matched 20% of what you raised for a total of $" + (amount * 1.2) + " . Awesome job!");
 }else if(time >= 10 && time <= 19){
     console.log("You ran for " + time + " minutes so we matched 10% of what you raised for a total of $" + (amount * 1.1) + " . Great job!");
 }else{
-
+    console.log("You ran for " + time + " minutes so you raised $" + amount + " . Try harder next year!");
 }
 
-//Ternary at end: prize = (totalRaised >= 100 ? "trophy" : "medal");
+prize = (amount >= 100 ? "trophy" : "medal");
+    console.log("You earned a " + prize +".");
