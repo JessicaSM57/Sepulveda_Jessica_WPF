557b3e2e6a2192ee1f925606fa647f8497eeca33
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-18 19:12:33 -0700

    Set up givens and started developing code.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,3 +1,19 @@
 //Jessica Sepulveda -- 03/18/14 -- Conditionals Assignment
 
-//Wacky
+//Wacky: How much wood would a woodchuck chuck if a woodchuck could chuck wood?
+//Given: An adult woodchuck can chuck 1 log of wood per 1 minute.
+//Given: Woodchuck average lifespan is 5 years; woodchucks under 1 year old and over 4 years old can chuck only half a log of wood per 1 minute.
+
+//Find out the age of the woodchuck.
+var howOld = prompt("How old is the woodchuck?")
+//NEED VALIDATION
+
+//Find out how much time will be allotted.
+var time = prompt("Enter the amount of time in minutes.");
+//NEED VALIDATION
+
+
+//If woodchuck is 4 years old or older, AND if woodchuck is 1 year old or younger
+if(howOld >= 4 || howOld <= 1){
+    console.log("A " + howOld + "year old woodchuck can chuck " + + "logs of wood in " + time + " minutes."); //The woodchuck can only chuck wood at a half rate.
+}
