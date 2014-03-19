c96b6464a4dd326b0368ef462b3b7a0a00f92dba
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-18 17:34:25 -0700

    Set up given.

---
+++ Conditionals Worksheet/js/script.js
@@ -0,0 +1,8 @@
+// Jessica Sepulveda -- 03/17/14 -- Conditionals Worksheet
+
+//Stuff your face I
+//Qualifying weight for heavyweight division of contest?
+
+var weight = 250; //Participant must be 250 pounds minimum to qualify
+
+
