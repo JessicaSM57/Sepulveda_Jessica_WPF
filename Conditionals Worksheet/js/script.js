6bf0f8649ab72fc16c563b905b614683bba48bc8
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-18 17:57:03 -0700

    Copied/pasted code worked out on another PC.

--- Conditionals Worksheet/js/script.js
+++ Conditionals Worksheet/js/script.js
@@ -1,21 +1,16 @@
 // Jessica Sepulveda -- 03/17/14 -- Conditionals Worksheet
 
-//Celsius to Fahrenheit converter
+//Check the Login
+//Correct username = student
+//Correct password = pass
 
-//Set up prompts as an array to collect 2 different pieces of information, numeric and alpha.
-var temp= [Number(prompt("Type in the numeric value of the temperature (i.e. 32).")), prompt("Type in capital F for Fahrenheit or capital C for Celsius.")];
+var userName = prompt("Please enter your username.");
+var password = prompt("Please enter your password.");
 
-//Set up temperature conversion calculations.
-var fToC = (temp[0] - 32) * 5 / 9;
-var cToF = temp[0] * 9 / 5 + 32;
-
-//If value entered for second prompt equals exactly capital F, then:
-if(temp[1] == "F"){
-    console.log("The temperature is " + fToC + " degrees Celsius.");
-
-//If value entered is NOT capital F then this code is executed:
+if(userName == "student" && password == "pass"){
+    console.log("Welcome, " + userName + "!");
+}else if(userName != "student" && password == "pass"){
+    console.log("User not found. Try again."); //if username does not match
 }else{
-    console.log("The temperature is " + cToF + " degrees Fahrenheit.");
+    console.log("Password does not match our records.");//if username matches but the password does not
 }
-
-//NOTES: Code would be more elegant if user could enter numeric+alpha value "32F" or "32f" and the values be separated and parsed correctly.
