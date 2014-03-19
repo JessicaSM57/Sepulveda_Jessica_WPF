056610ea022b091eea48bcfd42c9703410b8be93
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-18 22:51:38 -0700

    Trying to figure out logic for order of if else if else.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,22 +1,29 @@
 //Jessica Sepulveda -- 03/18/14 -- Conditionals Assignment
 
 //Personal: Fun Run
-//Given: Amount raised is matched by 10% for each 10 minutes that child runs. Max time to run allotted is 30 minutes.
+//Given: Amount raised is incrementally increased by 10% for each 10 minutes that child runs. Max time to run allotted is 30 minutes.
 
 //Set up variables that will be used in calculations.
 
 var amount = prompt("How much money did you raise?");
     //Validate that an entry is made:
     if(amount == ""){
-    amount = prompt("You did not enter a number. Please enter amount a whole numeric value.")
+    amount = prompt("You did not enter a number. Please enter amount a whole numeric value.");
     }
 
-var match = prompt("How long did you run?");
+var time = prompt("How long did you run?");
     //Validate that an entry is made:
-    if(match == ""){
-    match = prompt("You did not enter a number. Please enter minutes in a whole numeric value.")
+    if(time >=){
+    time = prompt("You did not enter a number. Please enter minutes in a whole numeric value.");
     }
 
+//Set up conditions to be applied
+if(time <= 9){
+    console.log("You ran for " + time + " minutes so you raised $" + amount + " . Try harder next year!");
+}else if(time >= 10 && time <= 19){
+    console.log("You ran for " + time + " minutes so we matched 10% of what you raised for a total of $" + (amount * 1.1) + " . Great job!");
+}else{
 
+}
 
 //Ternary at end: prize = (totalRaised >= 100 ? "trophy" : "medal");
