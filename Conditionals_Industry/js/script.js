fa36af0a9ee447f1984c9a8ec11d1f10c22b9a77
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-19 21:30:41 -0700

    Set up files and associated js.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,3 +1,5 @@
 //Jessica Sepulveda -- 03/18/14 -- Conditionals Assignment
 
-//Industry
+//Industry: Academic Probation, Warning, or Good Standing
+//Given: Minimum GPA requirement is 2.0
+//Pace = Cumulative number of hours (credit hours) that you have successfully completed divided by cumulative number of hours (credit hours) that you have attempted.
