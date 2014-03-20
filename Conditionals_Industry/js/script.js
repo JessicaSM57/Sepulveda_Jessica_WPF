879b04bc0ea3c8e1c6cbe63e10a184e4f03b0142
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-19 21:31:55 -0700

    Set up first variable as prompt and validated.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,3 +3,10 @@
 //Industry: Academic Probation, Warning, or Good Standing
 //Given: Minimum GPA requirement is 2.0
 //Pace = Cumulative number of hours (credit hours) that you have successfully completed divided by cumulative number of hours (credit hours) that you have attempted.
+
+//Set up variables that will be used in calculations and conditional statements.
+
+var gpa = prompt("What is your current Grade Point Average (GPA)?"); //User inputs their current GPA.
+if(gpa == ""){ //Validates that an entry was made by checking if prompt was left blank.
+    gpa = prompt("Please enter your GPA."); //Prompts user for information again.
+}
