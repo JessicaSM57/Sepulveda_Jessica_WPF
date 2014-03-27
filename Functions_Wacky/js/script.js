6a23283bc4c23455a68c2c6b79ff7a9233461ea4
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-26 22:05:43 -0700

    Created an array that will hold the answers to prompts.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,2 +1,6 @@
 //Functions_Wacky
 //Quiz: Will I like you as a friend?
+
+//Create an array to collect information. Given: "Yes" for all of the items will yield 100% results.
+var quiz = [prompt("For all prompts, type in yes or no. /br/ Do you like sci-fi movies?"), prompt("Do you like to read?"), prompt("Do you like to go hiking?"), prompt("Did you finish college?"), prompt("Do you like stories or movies about zombies?"), prompt("Do you have any kids?"), prompt("Have you heard of Battlestar Galactica?"), prompt("Are you currently employed?")];
+console.log(quiz);
