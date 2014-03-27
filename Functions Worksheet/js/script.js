a0e0223cfb63924fbc15bc441e9a75effadb8709
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-26 22:32:55 -0700

    Completed first part of Functions Worksheet on Mac.

---
+++ Functions Worksheet/js/script.js
@@ -0,0 +1,8 @@
+//Circumference of a Circle
+
+function calcCirc(radius) //parameter
+{
+    var calcCirc = radius * radius * 3.14; //calculation used to find circumference of a circle
+    console.log(calcCirc); //prints result to console
+}
+calcCirc(2); //calls function to run given a number for radius
