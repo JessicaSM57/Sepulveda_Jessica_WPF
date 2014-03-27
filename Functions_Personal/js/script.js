e5994b0364e55ce74359084f01f47a5f330dbad8
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-26 21:34:31 -0700

    Set up ternary using one logical operator.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -10,8 +10,12 @@ var time1 = prompt("How long did you exercise?");
 //Create a variable that will "catch" our return number
 var total = Math.round(calcFcal(age1, weight1, heartRate1, time1)); //This is calling the function and employs the 4 arguments established above as variables.
 
+//Set up ternary to print results to console.
+burnedCals = (total > 400 ? "Congratulations! You burned " + total + " calories." : "You only burned " + total + " calories. Try harder next time."); //Used conditional operator other than equals.
+console.log(burnedCals);
+
 ////Create function to calculate how many calories a woman burns.
 function calcFcal(age, weight, hearRate, time){ //Four parameters are established.
     var calories = ((age * 0.074) - (weight * 0.05741) + (hearRate * 0.4472) - 20.4022) * time / 4.184; //Calculation includes at least 2 factors as variables and uses 4 arithmetic operators.
-    return calories; //
+    return calories; //Saves result to be used when called.
 }
