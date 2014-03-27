146bc2806fcdad57a4bbdad0a739a3aab3f2a422
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-26 22:10:57 -0700

    Cleaned up code and deleted console.logs

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,12 +2,11 @@
 //Quiz: Will I like you as a friend?
 
 //Create an array to collect information. Given: "Yes" for all of the items will yield 100% results.
-var quiz = [prompt("For all prompts, type in yes or no. /br/ Do you like sci-fi movies?"), prompt("Do you like to read?"), prompt("Do you like to go hiking?"), prompt("Did you finish college?"), prompt("Do you like stories or movies about zombies?"), prompt("Do you have any kids?"), prompt("Have you heard of Battlestar Galactica?"), prompt("Are you currently employed?")];
+var quiz = [prompt("For all prompts, type in yes or no. \nDo you like sci-fi movies?"), prompt("Do you like to read?"), prompt("Do you like to go hiking?"), prompt("Did you finish college?"), prompt("Do you like stories or movies about zombies?"), prompt("Do you have any kids?"), prompt("Have you heard of Battlestar Galactica?"), prompt("Are you currently employed?")];
 console.log(quiz);
 
 var likeYou = 0;
 likeYou = likeAbility(quiz);
-console.log("Outside function" + likeYou);
 
 if(likeYou >= 4){
     console.log("Awesome! There is a " + likeYou / 8 * 100 + "% chance that I will like you.");
@@ -24,6 +23,5 @@ function likeAbility(answers){
         }
 
     }
-    console.log("Inside function" + numYes);
     return(numYes);
 }
