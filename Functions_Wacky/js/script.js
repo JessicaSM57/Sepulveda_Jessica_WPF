fa609b6c634579318865e2cda8a5ab4fd2c17ac4
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-26 22:06:35 -0700

    Made up a function that will read entries to array.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -4,3 +4,18 @@
 //Create an array to collect information. Given: "Yes" for all of the items will yield 100% results.
 var quiz = [prompt("For all prompts, type in yes or no. /br/ Do you like sci-fi movies?"), prompt("Do you like to read?"), prompt("Do you like to go hiking?"), prompt("Did you finish college?"), prompt("Do you like stories or movies about zombies?"), prompt("Do you have any kids?"), prompt("Have you heard of Battlestar Galactica?"), prompt("Are you currently employed?")];
 console.log(quiz);
+
+
+
+function likeAbility(answers){
+    console.log(answers[2]);
+    var numYes = 0;
+    for(var agree = 0; agree < answers.length; agree++){
+        if(answers[agree] == "yes"){
+            numYes++;
+        }
+
+    }
+    console.log("Inside function" + numYes);
+    return(numYes);
+}
