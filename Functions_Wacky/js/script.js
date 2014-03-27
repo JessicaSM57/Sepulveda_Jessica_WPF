dbc0150c3a0e9c1c83348d857189019dd77b6ffe
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-26 22:07:19 -0700

    Created a variable to catch the function's return value.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,6 +5,10 @@
 var quiz = [prompt("For all prompts, type in yes or no. /br/ Do you like sci-fi movies?"), prompt("Do you like to read?"), prompt("Do you like to go hiking?"), prompt("Did you finish college?"), prompt("Do you like stories or movies about zombies?"), prompt("Do you have any kids?"), prompt("Have you heard of Battlestar Galactica?"), prompt("Are you currently employed?")];
 console.log(quiz);
 
+var likeYou = 0;
+likeYou = likeAbility(quiz);
+console.log("Outside function" + likeYou);
+
 
 
 function likeAbility(answers){
