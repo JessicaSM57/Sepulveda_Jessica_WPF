26b08d2226f4a3fb55e8366e3e9d87b2cc791d9f
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-26 21:45:01 -0700

    Set up variables to use in function and code.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,9 @@
 //Functions_Industry
 //Profitability of a free-lance project in Web Developement
 //Formula: revenue = 1 - (cost / budget)
+
+//Set up variables that will be used in rest of code.
+var hours = prompt("Enter hours worked.");
+var perHour = prompt("Enter contracted rate per hour.");
+var other = prompt("Enter total for all other project costs. (i.e. travel)");
+var budget = prompt("Enter project budget.");
