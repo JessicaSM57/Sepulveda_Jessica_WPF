fdb527f950aaa9d89b948599e364abd4f3738bfb
JessicaSM57 <JessicaSM57@fullsail.edu>
2014-03-26 21:25:29 -0700

    Found calculation to use and set up variables for problem.

---
+++ Functions_Personal/js/script.js
@@ -0,0 +1,8 @@
+//How many calories will I burn?
+//Women: Calories Burned = ((Age x 0.074) - (Weight x 0.05741) + (Heart Rate x 0.4472) - 20.4022) x Time / 4.184 (Source: http://fitnowtraining.com/2012/01/formula-for-calories-burned/)
+
+//Establish variables that will be used in rest of code.
+var age1 = prompt("How old are you?");
+var weight1 = prompt("How much do you weigh?");
+var heartRate1 = prompt("Enter your average heart rate during the exercise.");
+var time1 = prompt("How long did you exercise?");
